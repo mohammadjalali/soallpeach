496284ac51eba31152529d0edaf5884b3cdf5ca6
Mohammad Hosein Jalali <mjalali1378@gmail.com>
2020-06-22 23:52:54 +0430

    count

--- countme/main.py
+++ countme/main.py
@@ -8,14 +8,14 @@ sumOfRequests = 0
 @app.route('/count')
 def count():
     global sumOfRequests
-    return str(sumOfRequests\n)
+    return str(sumOfRequests)+'\n'
 
 @app.route('/', methods=['POST'])
 def index():
     global sumOfRequests
     in_number = int(request.stream.read())
     sumOfRequests += in_number
-    return str(sumOfRequests\n)
+    return str(sumOfRequests)+'\n'
 
 if __name__ == '__main__':
     app.run(host=localhost, debug=True, port=80)
